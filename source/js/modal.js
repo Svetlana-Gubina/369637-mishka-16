436e31fd28f5d29e4b68e008ff7c9ec054ba694d
Svetlana <gubinas83@gmail.com>
2019-04-28 00:07:20 +0300

    Fix bugs, change header layout

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,46 +1,45 @@
 'use strict';
 (function () {
-	var overlay = document.querySelector(".modal-overlay");
-  var modal = document.querySelector(".modal");
-  var modalForm = document.querySelector(".modal__form");
-  var cartIcons = document.querySelectorAll('.catalog__item-cart-icon');
-  var promoButton = document.querySelector(".promo__button");
+var overlay = document.querySelector(".modal-overlay");
+var modal = document.querySelector(".modal");
+var modalForm = document.querySelector(".modal__form");
+var cartIcons = document.querySelectorAll('.catalog__item-cart-icon');
+var promoButton = document.querySelector(".promo__button");
 
- // Открытие и закрытие модального окна
- var addClickListener = function(button) {
-  button.addEventListener("click", function (evt) {
-		evt.preventDefault();
-		modalOpen(modal);
-		document.addEventListener('keydown', onPopupEscPress);
-	})
- };
+// Открытие и закрытие модального окна
+var addClickListener = function(button) {
+button.addEventListener("click", function (evt) {
+evt.preventDefault();
+modalOpen(modal);
+document.addEventListener('keydown', onPopupEscPress);
+})
+};
 
-	var modalClose = function (modal) {
-		modal.classList.remove("modal-show");
-		overlay.classList.remove("modal-show");
-	};
+var modalClose = function (modal) {
+modal.classList.remove("modal-show");
+overlay.classList.remove("modal-show");
+};
+var modalOpen = function (modal) {
+modal.classList.add("modal-show");
+overlay.classList.add("modal-show");
+};
 
-	var modalOpen = function (modal) {
-		modal.classList.add("modal-show");
-		overlay.classList.add("modal-show");
-  };
 
+var onPopupEscPress = function (evt) {
+if (evt.keyCode === 27) {
+modalClose(modal);
+}
+};
 
-	var onPopupEscPress = function (evt) {
-		if (evt.keyCode === 27) {
-			modalClose(modal);
-		}
-	};
+for (var i = 0; i < cartIcons.length; i++) {
+  var cartIcon = cartIcons[i];
+  addClickListener(cartIcon);
+};
 
-  for (var i = 0; i < cartIcons.length; i++) {
-    var cartIcon = cartIcons[i];
-    addClickListener(cartIcon);
-  };
+addClickListener(promoButton);
 
-  addClickListener(promoButton);
-
-  modalForm.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-    modalClose(modal);
-	});
+modalForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  modalClose(modal);
+});
 })();
